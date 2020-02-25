3eca24ba7a01eda36ece71fc8686fbc59196b1d1
Kirk Tolliver <kirktolliver28@gmail.com>
2020-02-24 19:31:08 -0600

    Added some more info

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,24 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-console.log(axios.get('https://api.github.com/users/maketaller'));
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
-
    Skip to Step 3.
 */
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
-const container = document.querySelector('.cards');
-const wholeContainer = document.querySelector('.container');
-axios.get('https://api.github.com/users/maketaller')
-  .then((resp) => {
-    container.appendChild(githubCard(resp));
-  });
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -31,32 +23,20 @@ axios.get('https://api.github.com/users/maketaller')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [
-  'https://api.github.com/users/Amber-Pittman',
-  'https://api.github.com/users/nickdurbin',
-  'https://api.github.com/users/leachcoding',
-  'https://api.github.com/users/clifhodges13',
-  'https://api.github.com/users/mnichols08'
-];
+axios.get('https://api.github.com/users/maketaller')
+.then(response => {
+  let container = document.querySelector('.cards');
+  container.append(createCard(response.data))
+  console.log(response)
+  console.log(createCard(response.data))})
+.catch(error => {
+  console.error(error)
+})
 
-const btn = document.createElement('button');
-btn.textContent = 'Show My Followers';
-btn.classList.add('show-more-btn');
-wholeContainer.appendChild(btn);
-
-btn.addEventListener('click', () => {
-  followersArray.forEach((item) => {
-    axios.get(item)
-      .then((resp) => {
-        container.appendChild(githubCard(resp));
-      })
-  })
-  btn.style.display = 'none';
-});
+const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
-
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -71,59 +51,7 @@ btn.addEventListener('click', () => {
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
-
-function githubCard(obj) {
-  const card = document.createElement('div');
-  card.classList.add('card');
-
-  const img = document.createElement('img');
-  img.src = obj.data.avatar_url;
-  card.appendChild(img);
-
-  const info = document.createElement('div');
-  info.classList.add('card-info');
-  card.appendChild(info);
-
-  const infoH3 = document.createElement('h3');
-  infoH3.classList.add('name');
-  infoH3.textContent = obj.data.name
-  info.appendChild(infoH3);
-
-  const user = document.createElement('p');
-  user.classList.add('username');
-  user.textContent = obj.data.login;
-  info.appendChild(user);
-
-  const location = document.createElement('p');
-  location.textContent = obj.data.location;
-  info.appendChild(location);
-
-  const profile = document.createElement('p');
-  profile.textContent = 'Profile:';
-  info.appendChild(profile);
-
-  const profileLink = document.createElement('a');
-  profileLink.href = obj.data.html_url;
-  profileLink.textContent = ` ${obj.data.html_url}`
-  profile.appendChild(profileLink);
-
-  const followers = document.createElement('p');
-  followers.textContent = `Followers: ${obj.data.followers}`;
-  info.appendChild(followers);
-
-  const following = document.createElement('p');
-  following.textContent = `Following: ${obj.data.following}`;
-  info.appendChild(following);
-
-  const bio = document.createElement('p');
-  bio.textContent = obj.data.bio;
-  info.append(bio);
-
-
-  return card;
-}
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -132,3 +60,54 @@ function githubCard(obj) {
   luishrd
   bigknell
 */
+// let content = ;
+// console.log(content);
+
+const createCard = function(user) {
+  let card = document.createElement('div');
+  card.classList.add('card');
+
+  let image = document.createElement('img');
+  image.src = user['avatar_url'];
+
+  let cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+
+  let name = document.createElement('h3');
+  name.classList.add('name');
+  name.textContent = user.name || 'See Username'
+
+
+  let username = document.createElement('p');
+  username.classList.add('username');
+  username.textContent = user.login;
+
+  let location = document.createElement('p');
+  location.textContent = `Location: ${user.location}` || 'Not Available'
+
+  let profile = document.createElement('p');
+
+  let followers = document.createElement('p');
+  followers.textContent = `Followers: ${user.followers}`
+
+  let following = document.createElement('p');
+
+  let bio = document.createElement('p');
+
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+
+  const cardSection = document.querySelector('.cards');
+
+
+  return card
+}
+// console.log(createCard(re))
